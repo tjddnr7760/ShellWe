7e70cb12cae45d6f04afb6250b50652438a93de3
Mason3144 <lts890303@gmail.com>
2023-07-02 18:19:24 +0900

    [init] build.gradle, application.yml 초기 설정

---
+++ server/src/test/java/com/shellwe/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shellwe.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
