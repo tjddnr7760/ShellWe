3f7e15add2927687b6598be1364c3cd613f531d2
cs2199 <tjddnr7760@naver.com>
2023-07-04 07:00:53 +0900

    [feat] 회원가입 컨트롤러 및 그 외 기본 구조 구현 (#12)

--- server/src/main/java/com/shellwe/server/domain/member/controller/MemberController.java
+++ server/src/main/java/com/shellwe/server/domain/member/controller/MemberController.java
@@ -1,2 +1,59 @@
-package com.shellwe.server.domain.member.controller;public class MemberController {
+package com.shellwe.server.domain.member.controller;
+
+import com.shellwe.server.domain.member.dto.request.DeleteRequestDto;
+import com.shellwe.server.domain.member.dto.request.SignUpRequestDto;
+import com.shellwe.server.domain.member.dto.request.UpdateRequestDto;
+import com.shellwe.server.domain.member.dto.response.FindResponseDto;
+import com.shellwe.server.domain.member.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequestMapping("/members")
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void signUpMember(@RequestBody SignUpRequestDto signUpRequestDto) {
+        memberService.signUpMember(signUpRequestDto);
+    }
+
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/{memberId}")
+//    public FindResponseDto getMyMemberById(Authentication authentication,
+//                                           @PathVariable long memberId) {
+//        FindResponseDto memberById = memberService.findMemberById(memberId);
+//        return memberById;
+//    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{memberId}")
+    public FindResponseDto getOtherMemberById(@PathVariable long memberId) {
+        FindResponseDto memberById = memberService.findMemberById(memberId);
+        return memberById;
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/{memberId}")
+    public void updateMemberById(@PathVariable long memberId,
+                                 @RequestBody UpdateRequestDto updateRequestDto) {
+        memberService.updateMember("email", memberId, updateRequestDto);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{memberId}")
+    public void deleteMemberByIdAndPassword(@PathVariable long memberId,
+                                            @RequestBody DeleteRequestDto deleteRequestDto) {
+        memberService.deleteMember("email", memberId, deleteRequestDto);
+    }
 }
