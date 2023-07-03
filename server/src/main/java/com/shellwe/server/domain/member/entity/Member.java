fde4c185b8333a20457586c83d47adc93d1f74bd
cs2199 <tjddnr7760@naver.com>
2023-07-04 07:00:08 +0900

    [feat] mapstruct사용을 위한 빌더 추가 (#12)

--- server/src/main/java/com/shellwe/server/domain/member/entity/Member.java
+++ server/src/main/java/com/shellwe/server/domain/member/entity/Member.java
@@ -3,14 +3,17 @@ package com.shellwe.server.domain.member.entity;
 import com.shellwe.server.domain.shell.entity.Shell;
 import com.shellwe.server.utils.TimeTracker;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Getter
 @Table(name = "MEMBER")
 @Entity
@@ -28,7 +31,24 @@ public class Member extends TimeTracker {
     private String displayName;
 
     @OneToMany(mappedBy = "member")
-    private List<Shell> shells;
+    private List<Shell> shells = new ArrayList<>();
+
+    public Member(Member member, String password) {
+        this.email = member.getEmail();
+        this.displayName = member.getDisplayName();
+        this.shells = member.getShells();
+        this.password = password;
+    }
+
+    @Builder
+    public Member(String email, String password, String displayName, List<Shell> shells) {
+        this.email = email;
+        this.password = password;
+        this.displayName = displayName;
+        if (shells != null) {
+            this.shells = shells;
+        }
+    }
 
     public void addShell(Shell shell) {
         this.shells.add(shell);
