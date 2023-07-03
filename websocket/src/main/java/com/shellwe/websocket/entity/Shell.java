8b0de781a3d2be059b6174f36e7fe59926c6997e
Mason3144 <lts890303@gmail.com>
2023-07-03 15:50:12 +0900

    [feat] WebSocket서버 DB 연결 (#5)

--- websocket/src/main/java/com/shellwe/websocket/entity/Shell.java
+++ websocket/src/main/java/com/shellwe/websocket/entity/Shell.java
@@ -1,39 +1,4 @@
 package com.shellwe.websocket.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import javax.validation.constraints.Size;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Builder
-@Table(name = "SHELL")
-@Entity
-public class Shell extends TimeTracker {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "SHELL_ID")
-    private Long id;
-
-    @Column(name = "TITLE", length = 60)
-    @Size(max = 30)
-    private String title;
-
-    @Column(name = "BODY", columnDefinition = "TEXT")
-    @Size(max = 1500)
-    private String body;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "MEMBER_ID")
-    private Member member;
-
-    public Shell(Long id) {
-        this.id = id;
-    }
+public class Shell {
 }
