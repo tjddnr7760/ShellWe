2649a1e87108e3cccec5d0d60e47615593fcb4d1
cs2199 <tjddnr7760@naver.com>
2023-07-03 22:25:50 +0900

    [feat] 멤버 리포지토리 추가 (#12)

--- server/src/main/java/com/shellwe/server/domain/member/repository/MemberRepository.java
+++ server/src/main/java/com/shellwe/server/domain/member/repository/MemberRepository.java
@@ -1,2 +1,10 @@
-package com.shellwe.server.domain.member.repository;public class MemberRepository {
+package com.shellwe.server.domain.member.repository;
+
+import com.shellwe.server.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
