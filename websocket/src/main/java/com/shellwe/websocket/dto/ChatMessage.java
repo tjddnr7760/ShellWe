8f8b0f2f879f2ae5755c59e2e270656e490e92b1
Mason3144 <lts890303@gmail.com>
2023-07-03 23:06:36 +0900

    [feat] rooms 리스트 조회 (#5)

--- websocket/src/main/java/com/shellwe/websocket/dto/ChatMessage.java
+++ websocket/src/main/java/com/shellwe/websocket/dto/ChatMessage.java
@@ -11,7 +11,7 @@ public class ChatMessage {
         ENTER, TALK
     }
     private MessageType type; // 메시지 타입
-    private String roomId; // 방번호
+    private Long roomId; // 방번호
     private String sender; // 메시지 보낸사람
     private String message; // 메시지
 }
