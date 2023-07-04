65c98b7d47089bbcc7d0fcee673fa39b17bea44b
cs2199 <tjddnr7760@naver.com>
2023-07-04 20:42:21 +0900

    [feat] 토큰 인증 실패시, 이메일 인증 실패시 응답되는 리스폰스 핸들러 (#12)

--- server/src/main/java/com/shellwe/server/auth/handler/tokenVerificationFailedHandler.java
+++ server/src/main/java/com/shellwe/server/auth/handler/tokenVerificationFailedHandler.java
@@ -1,2 +1,29 @@
-package com.shellwe.server.auth.handler;public class tokenVerificationFailedHandler {
+package com.shellwe.server.auth.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class tokenVerificationFailedHandler implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        Exception exception = (Exception) request.getAttribute("exception");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        logExceptionMessage(authException, exception);
+    }
+
+    private void logExceptionMessage(AuthenticationException authException, Exception exception) {
+        String message = exception != null ? exception.getMessage() : authException.getMessage();
+        log.warn("Unauthorized error happened: {}", message);
+    }
 }
