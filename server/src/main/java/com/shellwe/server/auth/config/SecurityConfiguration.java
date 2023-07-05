6f1c283e0f0515eb22157c7a3f68fdac6682bbb1
cs2199 <tjddnr7760@naver.com>
2023-07-05 11:51:17 +0900

    [feat] oauth 필터 설정 (#12)

--- server/src/main/java/com/shellwe/server/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/shellwe/server/auth/config/SecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.shellwe.server.auth.config;
 
 import com.shellwe.server.auth.filter.JwtAuthenticationFilter;
 import com.shellwe.server.auth.filter.JwtVerificationFilter;
-import com.shellwe.server.auth.handler.loginAuthenticationFailureHandler;
-import com.shellwe.server.auth.handler.loginAuthenticationSuccessHandler;
-import com.shellwe.server.auth.handler.tokenVerificationFailedHandler;
-import com.shellwe.server.auth.handler.verificationAuthFailedHandler;
+import com.shellwe.server.auth.handler.jwt.LoginAuthenticationFailureHandler;
+import com.shellwe.server.auth.handler.jwt.LoginAuthenticationSuccessHandler;
+import com.shellwe.server.auth.handler.jwt.TokenVerificationFailedHandler;
+import com.shellwe.server.auth.handler.jwt.VerificationAuthFailedHandler;
+import com.shellwe.server.auth.handler.oauth.OAuth2MemberFailureHandler;
+import com.shellwe.server.auth.handler.oauth.OAuth2MemberSuccessHandler;
 import com.shellwe.server.auth.jwt.JwtTokenizer;
+import com.shellwe.server.domain.member.service.OAuthMemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -18,6 +21,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -30,9 +34,11 @@ import java.util.Arrays;
 public class SecurityConfiguration {
 
     private final JwtTokenizer jwtTokenizer;
+    private final OAuthMemberService oAuthMemberService;
 
-    public SecurityConfiguration(JwtTokenizer jwtTokenizer) {
+    public SecurityConfiguration(JwtTokenizer jwtTokenizer, OAuthMemberService oAuthMemberService) {
         this.jwtTokenizer = jwtTokenizer;
+        this.oAuthMemberService = oAuthMemberService;
     }
 
     @Bean
@@ -67,42 +73,46 @@ public class SecurityConfiguration {
 
             JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(authenticationManager, jwtTokenizer);
             jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
-            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new loginAuthenticationSuccessHandler());
-            jwtAuthenticationFilter.setAuthenticationFailureHandler(new loginAuthenticationFailureHandler());
+            jwtAuthenticationFilter.setAuthenticationSuccessHandler(new LoginAuthenticationSuccessHandler());
+            jwtAuthenticationFilter.setAuthenticationFailureHandler(new LoginAuthenticationFailureHandler());
 
             JwtVerificationFilter jwtVerificationFilter = new JwtVerificationFilter(jwtTokenizer);
 
             builder
                     .addFilter(jwtAuthenticationFilter)
-                    .addFilterAfter(jwtVerificationFilter, JwtAuthenticationFilter.class);
+                    .addFilterAfter(jwtVerificationFilter, OAuth2LoginAuthenticationFilter.class);
         }
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .headers().frameOptions().sameOrigin()
+                    .headers().frameOptions().sameOrigin()
                 .and()
-                .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource())
+                    .csrf().disable()
+                    .cors().configurationSource(corsConfigurationSource())
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
-                .formLogin().disable()
-                .httpBasic().disable()
-                .exceptionHandling()
-                .authenticationEntryPoint(new tokenVerificationFailedHandler())
-                .accessDeniedHandler(new verificationAuthFailedHandler())
+                    .formLogin().disable()
+                    .httpBasic().disable()
+                    .exceptionHandling()
+                    .authenticationEntryPoint(new TokenVerificationFailedHandler())
+                    .accessDeniedHandler(new VerificationAuthFailedHandler())
                 .and()
-                .apply(new CustomFilterConfigurer())
+                    .apply(new CustomFilterConfigurer())
                 .and()
-                .authorizeHttpRequests(authorize -> authorize
-                        .mvcMatchers(HttpMethod.GET, "/**").permitAll()
-                        .mvcMatchers(HttpMethod.POST, "/members").permitAll()
-                        .mvcMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .mvcMatchers("/**").hasAuthority("EMAIL_VERIFIED")
-                        .anyRequest().authenticated()
-                );
+                    .authorizeHttpRequests(authorize -> authorize
+                            .mvcMatchers(HttpMethod.GET, "/**").permitAll()
+                            .mvcMatchers(HttpMethod.POST, "/members").permitAll()
+                            .mvcMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                            .mvcMatchers("/**").hasAuthority("EMAIL_VERIFIED")
+                            .anyRequest().authenticated()
+                    )
+                    .oauth2Login(oauth2 -> oauth2
+                            .successHandler(new OAuth2MemberSuccessHandler(jwtTokenizer, oAuthMemberService))
+                            .failureHandler(new OAuth2MemberFailureHandler())
+                    );
 
         return http.build();
     }
