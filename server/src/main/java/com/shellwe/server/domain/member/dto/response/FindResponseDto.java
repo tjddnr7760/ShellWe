749a4de53c878a7c79db35fd648a53313179a53b
cs2199 <tjddnr7760@naver.com>
2023-07-05 15:44:27 +0900

    [feat] 유효성 검사 추가, isMe json 이름 변경 (#12)

--- server/src/main/java/com/shellwe/server/domain/member/dto/response/FindResponseDto.java
+++ server/src/main/java/com/shellwe/server/domain/member/dto/response/FindResponseDto.java
@@ -1,5 +1,6 @@
 package com.shellwe.server.domain.member.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Setter
 public class FindResponseDto {
 
+    @JsonProperty("isMe")
     private boolean isMe;
 
     private Long memberId;
@@ -14,4 +16,10 @@ public class FindResponseDto {
     private String displayName;
 
     // 이미지 필드 현재단계 제외
+
+    public void setMeIdName(boolean isMe, Long memberId, String displayName) {
+        this.isMe = isMe;
+        this.memberId = memberId;
+        this.displayName = displayName;
+    }
 }
