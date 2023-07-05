74fae3e8c8ace27166aca117c53946a58bb2fbad
cs2199 <tjddnr7760@naver.com>
2023-07-05 11:50:42 +0900

    [feat] oauth전용 멤버 서비스 로직 구현 (#12)

--- server/src/main/java/com/shellwe/server/domain/member/service/OAuthMemberService.java
+++ server/src/main/java/com/shellwe/server/domain/member/service/OAuthMemberService.java
@@ -1,2 +1,42 @@
-package com.shellwe.server.domain.member.service;public class OAuthMemberService {
+package com.shellwe.server.domain.member.service;
+
+import com.shellwe.server.domain.member.entity.Member;
+import com.shellwe.server.domain.member.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+public class OAuthMemberService {
+
+    private final MemberRepository memberRepository;
+
+    public OAuthMemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void oauthSignUpMember(Member member) {
+        log.info("sign-up in service layer by oauth start, member : {}", member);
+
+        if (verifyExistEmailByOauth(member.getEmail())) {
+            log.info("exist member in oauth");
+            return;
+        }
+        log.info("member password null");
+        memberRepository.save(member);
+
+        log.info("sign-up in service layer done");
+    }
+
+    private boolean verifyExistEmailByOauth(String email) {
+        boolean exist = false;
+        Optional<Member> member = memberRepository.findByEmail(email);
+
+        if (member.isPresent()) {
+            exist = true;
+        }
+        return exist;
+    }
 }
