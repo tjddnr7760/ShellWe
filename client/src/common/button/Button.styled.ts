5d2c5e02b567dc38cd5b75920bed0960d5452f6d
Jeongchanyeong <135792468020@naver.com>
2023-07-06 16:15:55 +0900

    [feat] Button style (#46)

---
+++ client/src/common/button/Button.styled.ts
@@ -0,0 +1,167 @@
+import styled from 'styled-components';
+
+export const SmallButton1 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  color: #023e8a;
+  border: 0.5px solid #eeee;
+  cursor: pointer;
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const SmallButton2 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: #b9eaff;
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const SmallButton3 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: #90e0ef;
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  border: 0.1px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #91e9ff;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const SmallButton4 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: #00b4d8;
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  cursor: pointer;
+
+  border: 0.5px solid white;
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const SmallButton5 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: #0077b6;
+  border-radius: 16px;
+  font-weight: 300;
+  font-size: 16px;
+  color: white;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const SmallButton6 = styled.button`
+  width: 90px;
+  height: 50px;
+  background-color: #023e8a;
+  border-radius: 16px;
+  color: white;
+  font-size: 16px;
+  font-weight: 300;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const BigButton1 = styled.button`
+  width: 390px;
+  height: 70px;
+  background-color: #90e0ef;
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #91e9ff;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const BigButton2 = styled.button`
+  width: 390px;
+  height: 70px;
+  background-color: #00b4d8;
+  border-radius: 16px;
+  font-size: 16px;
+  font-weight: 300;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
+export const BigButton3 = styled.button`
+  width: 390px;
+  height: 70px;
+  background-color: #0077b6;
+  border-radius: 16px;
+  font-weight: 300;
+  font-size: 16px;
+  color: white;
+  border: 0.5px solid white;
+  cursor: pointer;
+
+  &:hover {
+    color: #023e8a;
+    background-color: #90e0ef;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
