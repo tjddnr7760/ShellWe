cd91a8471eeaf9dad24e51c704037c0571041e47
cs2199 <tjddnr7760@naver.com>
2023-07-06 23:50:24 +0900

    [feat] talent, product 카테고리 합침 (#59)

--- server/src/main/java/com/shellwe/server/domain/types/category/ShellCategory.java
+++ server/src/main/java/com/shellwe/server/domain/types/category/ShellCategory.java
@@ -1,2 +1,6 @@
-package com.shellwe.server.domain.types.category;public enum ShellCategory {
+package com.shellwe.server.domain.types.category;
+
+public enum ShellCategory {
+    P_ALL, P_DEVICE, P_FOOD, P_HEALTH, P_OFFICE, P_LIFE, P_ETC,
+    T_ALL, T_TECH, T_HEALTH, T_HOME, T_ART, T_LANG, T_ETC
 }
