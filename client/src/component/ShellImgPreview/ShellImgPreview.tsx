57d7b5efa4802f67bc7cf183e8549cee5e227425
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-06 13:30:45 +0900

    [feat] 쉘 이미지 미리보기 UI 완료 #14

---
+++ client/src/component/ShellImgPreview/ShellImgPreview.tsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  ImageWrapper,
+  LeftSlideButton,
+  RightSlideButton,
+  ShellImg,
+  ShellImgBox,
+} from './ShellImgPreview.styled';
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import product from '../../asset/product.jpg';
+
+const ShellImgPreview = () => {
+  return (
+    <ImageWrapper>
+      <ShellImgBox>
+        <ShellImg src={product} alt="sshell-image"></ShellImg>
+        {/* src={GET 요청 후, data에서 Shell 이미지 받아서 할당 } */}
+      </ShellImgBox>
+      <LeftSlideButton>
+        <FontAwesomeIcon icon={faAngleLeft} style={{ color: '#0077b6' }} />
+      </LeftSlideButton>
+      <RightSlideButton>
+        <FontAwesomeIcon icon={faAngleRight} style={{ color: '#0077b6' }} />
+      </RightSlideButton>
+    </ImageWrapper>
+  );
+};
+
+export default ShellImgPreview;
