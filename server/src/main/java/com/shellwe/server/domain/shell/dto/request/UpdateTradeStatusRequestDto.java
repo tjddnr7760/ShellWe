36273eb10989519c058474b2330b9c71d8b97b3a
cs2199 <tjddnr7760@naver.com>
2023-07-06 23:52:31 +0900

    [feat] shell 컨트롤러, dto 구조 설계 (#59)

--- server/src/main/java/com/shellwe/server/domain/shell/dto/request/UpdateTradeStatusRequestDto.java
+++ server/src/main/java/com/shellwe/server/domain/shell/dto/request/UpdateTradeStatusRequestDto.java
@@ -1,4 +1,12 @@
-package com.shellwe.server.domain.member.dto.request;
+package com.shellwe.server.domain.shell.dto.request;
 
+import com.shellwe.server.domain.types.Status;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UpdateTradeStatusRequestDto {
+
+    private Status status;
 }
--- server/src/main/java/com/shellwe/server/domain/shell/dto/response/InquiryToMainResponseDto.java
+++ server/src/main/java/com/shellwe/server/domain/shell/dto/response/InquiryToMainResponseDto.java
@@ -1,4 +1,13 @@
-package com.shellwe.server.domain.member.dto.response;
+package com.shellwe.server.domain.shell.dto.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class InquiryToMainResponseDto {
+
+    private List<ShellResponseDto> shells;
 }
