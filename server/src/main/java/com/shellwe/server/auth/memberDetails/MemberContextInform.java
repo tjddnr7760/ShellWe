ab41320b928c6c8efd451689a7b54d4a940518c0
cs2199 <tjddnr7760@naver.com>
2023-07-06 11:24:53 +0900

    [feat] 컨텍스트 정보 저장 전용 객체 추가 (#12)

--- server/src/main/java/com/shellwe/server/auth/memberDetails/MemberContextInform.java
+++ server/src/main/java/com/shellwe/server/auth/memberDetails/MemberContextInform.java
@@ -1,2 +1,17 @@
-package com.shellwe.server.auth.memberDetails;public class MemberContextInform {
+package com.shellwe.server.auth.memberDetails;
+
+import lombok.Getter;
+
+@Getter
+public class MemberContextInform {
+    private String email;
+    private String displayName;
+    private String id;
+    // 이미지는 도메인 구현 후 추가
+
+    public MemberContextInform(String email, String displayName, String id) {
+        this.email = email;
+        this.displayName = displayName;
+        this.id = id;
+    }
 }
