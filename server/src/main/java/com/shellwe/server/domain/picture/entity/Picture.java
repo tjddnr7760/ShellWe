8fd6f752a47fbdcd8ef7bdd3011f60c77791ac53
cs2199 <tjddnr7760@naver.com>
2023-07-06 23:48:51 +0900

    [refactor] picture 도메인 이름 수정 (#59)

--- server/src/main/java/com/shellwe/server/domain/picture/entity/Picture.java
+++ server/src/main/java/com/shellwe/server/domain/picture/entity/Picture.java
@@ -1,6 +1,5 @@
 package com.shellwe.server.domain.picture.entity;
 
-import com.shellwe.server.domain.member.entity.Member;
 import com.shellwe.server.domain.shell.entity.Shell;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,13 +10,13 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Table(name = "PHOTO")
+@Table(name = "PICTURE")
 @Entity
 public class Picture {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "PHOTO_ID")
+    @Column(name = "PICTURE_ID")
     private Long id;
 
     private String url;
