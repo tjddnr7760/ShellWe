6715996d39ee3e1f53a676d9a1260305ff237cbe
cs2199 <tjddnr7760@naver.com>
2023-07-07 09:40:22 +0900

    [feat] 쉘 서비스, 컨트롤러, dto 추가작업 (#59)

--- server/src/main/java/com/shellwe/server/domain/shell/controller/ShellController.java
+++ server/src/main/java/com/shellwe/server/domain/shell/controller/ShellController.java
@@ -1,5 +1,6 @@
 package com.shellwe.server.domain.shell.controller;
 
+import com.shellwe.server.auth.memberDetails.MemberContextInform;
 import com.shellwe.server.domain.shell.dto.request.RegisterRequestDto;
 import com.shellwe.server.domain.shell.dto.request.UpdateRequestDto;
 import com.shellwe.server.domain.shell.dto.request.UpdateTradeStatusRequestDto;
@@ -15,6 +16,7 @@ import com.shellwe.server.domain.types.ShellType;
 import com.shellwe.server.domain.types.category.ShellCategory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,53 +32,73 @@ public class ShellController {
         this.shellService = shellService;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public RegisterResponseDto registerShell(@RequestBody RegisterRequestDto registerRequestDto, Authentication authentication) {
-
+        return shellService.register(registerRequestDto, getId(authentication));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{shellId}")
-    public FindDetailsResponseDto findShellDetails(@PathVariable long shellId) {
-
+    public FindDetailsResponseDto findShellDetails(@PathVariable long shellId, Authentication authentication) {
+        return shellService.findDetails(shellId, getId(authentication));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{shellId}/update")
     public UpdateResponseDto updateShell(@PathVariable long shellId,
                                          @RequestBody UpdateRequestDto updateRequestDto,
                                          Authentication authentication) {
-
+        return shellService.update(shellId, updateRequestDto, getId(authentication));
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{shellId}")
     public void deleteShell(@PathVariable long shellId, Authentication authentication) {
-
+        shellService.delete(shellId, getId(authentication));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{shellId}")
     public UpdateTradeStatusResponseDto updateShellTradeStatus(@PathVariable long shellId,
                                                                @RequestBody UpdateTradeStatusRequestDto updateTradeStatusRequestDto,
                                                                Authentication authentication) {
-
+        return shellService.updateTradeStatus(shellId, updateTradeStatusRequestDto, getId(authentication));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/main")
     public InquiryToMainResponseDto inquiryShellsToMain(@RequestParam("size") int size) {
-
+        return shellService.inquiryToMain(size);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/shells")
     public InquiryResponseDto inquiryShells(@RequestParam("limit") int limit,
                                             @RequestParam("cursor") int cursor,
                                             @RequestParam("type") ShellType shellType,
                                             @RequestParam("category") ShellCategory shellCategory) {
-
+        return shellService.inquiry(limit, cursor, shellType, shellCategory);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
     public SearchResponseDto searchShells(@RequestParam("limit") int limit,
                                           @RequestParam("cursor") int cursor,
                                           @RequestParam("type") ShellType shellType,
                                           @RequestParam("title") String title) {
+        return shellService.search(limit, cursor, shellType, title);
+    }
 
+    private Long getId(Authentication authentication) {
+        Long id;
+        if (authentication == null) {
+            id = null;
+        } else {
+            MemberContextInform memberInform = (MemberContextInform) authentication.getPrincipal();
+            id = memberInform.getId();
+        }
+        System.out.println("id = " + id);
+        return id;
     }
 }
--- server/src/main/java/com/shellwe/server/domain/shell/service/ShellService.java
+++ server/src/main/java/com/shellwe/server/domain/shell/service/ShellService.java
@@ -1,7 +1,13 @@
 package com.shellwe.server.domain.shell.service;
 
+import com.shellwe.server.domain.shell.dto.request.RegisterRequestDto;
+import com.shellwe.server.domain.shell.dto.request.UpdateRequestDto;
+import com.shellwe.server.domain.shell.dto.request.UpdateTradeStatusRequestDto;
+import com.shellwe.server.domain.shell.dto.response.*;
 import com.shellwe.server.domain.shell.mapper.ShellMapper;
 import com.shellwe.server.domain.shell.repository.ShellRepository;
+import com.shellwe.server.domain.types.ShellType;
+import com.shellwe.server.domain.types.category.ShellCategory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +25,65 @@ public class ShellService {
         this.shellMapper = shellMapper;
     }
 
+    public RegisterResponseDto register(RegisterRequestDto registerRequestDto, long memberId) {
+        // dto를 shell 객체로 변환한다.
 
+        // shell의 정보를 셋팅하고 member를 등록한다.
+
+        // shell을 db에 저장한다.
+
+        // dto로 변환해서 리턴한다.
+    }
+
+    public FindDetailsResponseDto findDetails(long shellId, Long memberId) {
+        // shellid로 특정 shell을 조회한다.
+
+        // memberId를 비교하고 자기자신이면 isMe를 true로 변경한다. memberId가 null이면 비로그인 정보 조회이다.
+
+        // dto로 바꿔서 리턴한다.
+    }
+
+    public UpdateResponseDto update(long shellId, UpdateRequestDto updateRequestDto, long memberId) {
+        // shellId로 특정 shell을 조회한다.
+
+        // 유저가 memberId와 동일한지 검증한다.
+
+        // 동일하면 업데이트하고 dto를 리턴한다.
+    }
+
+    public void delete(long shellId, long id) {
+        // shellId로 특정 shell을 조회한다.
+
+        // 유저가 memberId와 동일한지 검증한다.
+
+        // 동일하면 삭제하고 dto를 리턴한다.
+    }
+
+    public UpdateTradeStatusResponseDto updateTradeStatus(long shellId, UpdateTradeStatusRequestDto updateTradeStatusRequestDto, long memberId) {
+        // shellId로 특정 shell을 조회한다.
+
+        // 유저가 memberId와 동일한지 검증한다.
+
+        // dto에 저장된 정보대로 저장한다.
+    }
+
+    public InquiryToMainResponseDto inquiryToMain(int size) {
+        // size만큼 거래정보 많은 쉘순서대로 List로 가져온다.
+
+        // 리스트를 각각 findshells에서 처리해주고
+
+        // dto로 변환해서 리턴한다.
+    }
+
+    public InquiryResponseDto inquiry(int limit, int cursor, ShellType shellType, ShellCategory shellCategory) {
+        // 커서 형태로 최대 limit만큼 특정 shelltype의 특정 카테고리 shell을 불러온다.
+
+        // 리스트를 각각 findshells에서 처리해주고
+
+        // dto로 변환해서 리턴한다.
+    }
+
+    public SearchResponseDto search(int limit, int cursor, String title) {
+        // shelltype? -> 타이틀로 검색
+    }
 }
