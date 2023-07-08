de29b69f4d92721236e48669ff4d6df6a9990f14
cs2199 <tjddnr7760@naver.com>
2023-07-09 08:57:18 +0900

    [feat] Shell 도메인 컨트롤러, 서비스, 매퍼, 엔티티 구현 (#59)

--- server/src/main/java/com/shellwe/server/domain/shell/controller/ShellController.java
+++ server/src/main/java/com/shellwe/server/domain/shell/controller/ShellController.java
@@ -5,7 +5,6 @@ import com.shellwe.server.domain.shell.dto.request.RegisterRequestDto;
 import com.shellwe.server.domain.shell.dto.request.UpdateRequestDto;
 import com.shellwe.server.domain.shell.dto.request.UpdateTradeStatusRequestDto;
 import com.shellwe.server.domain.shell.dto.response.InquiryResponseDto;
-import com.shellwe.server.domain.shell.dto.response.InquiryToMainResponseDto;
 import com.shellwe.server.domain.shell.dto.response.SearchResponseDto;
 import com.shellwe.server.domain.shell.dto.response.UpdateTradeStatusResponseDto;
 import com.shellwe.server.domain.shell.dto.response.FindDetailsResponseDto;
@@ -19,6 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Slf4j
 @RequestMapping("/shells")
@@ -34,8 +36,10 @@ public class ShellController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public RegisterResponseDto registerShell(@RequestBody RegisterRequestDto registerRequestDto, Authentication authentication) {
-        return shellService.register(registerRequestDto, getId(authentication));
+    public RegisterResponseDto registerShell(@RequestPart("register") RegisterRequestDto registerRequestDto,
+                                             @RequestPart("pictures") List<MultipartFile> pictures,
+                                             Authentication authentication) {
+        return shellService.register(registerRequestDto, getId(authentication), pictures);
     }
 
     @ResponseStatus(HttpStatus.OK)
@@ -47,9 +51,10 @@ public class ShellController {
     @ResponseStatus(HttpStatus.OK)
     @PatchMapping("/{shellId}/update")
     public UpdateResponseDto updateShell(@PathVariable long shellId,
-                                         @RequestBody UpdateRequestDto updateRequestDto,
+                                         @RequestPart("update") UpdateRequestDto updateRequestDto,
+                                         @RequestPart("picture") MultipartFile picture,
                                          Authentication authentication) {
-        return shellService.update(shellId, updateRequestDto, getId(authentication));
+        return shellService.update(shellId, updateRequestDto, getId(authentication), picture);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
@@ -66,29 +71,29 @@ public class ShellController {
         return shellService.updateTradeStatus(shellId, updateTradeStatusRequestDto, getId(authentication));
     }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/main")
-    public InquiryToMainResponseDto inquiryShellsToMain(@RequestParam("size") int size) {
-        return shellService.inquiryToMain(size);
-    }
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/main")
+//    public InquiryToMainResponseDto inquiryShellsToMain(@RequestParam("size") int size) {
+//        return shellService.inquiryToMain(size);
+//    }
+//    트레이드 도메인에서 구현
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/shells")
-    public InquiryResponseDto inquiryShells(@RequestParam("limit") int limit,
-                                            @RequestParam("cursor") int cursor,
-                                            @RequestParam("type") ShellType shellType,
-                                            @RequestParam("category") ShellCategory shellCategory) {
-        return shellService.inquiry(limit, cursor, shellType, shellCategory);
-    }
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/shells")
+//    public InquiryResponseDto inquiryShells(@RequestParam("limit") int limit,
+//                                            @RequestParam("cursor") int cursor,
+//                                            @RequestParam("type") ShellType shellType,
+//                                            @RequestParam("category") ShellCategory shellCategory) {
+//        return shellService.inquiry(limit, cursor, shellType, shellCategory);
+//    }
 
-    @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/search")
-    public SearchResponseDto searchShells(@RequestParam("limit") int limit,
-                                          @RequestParam("cursor") int cursor,
-                                          @RequestParam("type") ShellType shellType,
-                                          @RequestParam("title") String title) {
-        return shellService.search(limit, cursor, shellType, title);
-    }
+//    @ResponseStatus(HttpStatus.OK)
+//    @GetMapping("/search")
+//    public SearchResponseDto searchShells(@RequestParam("limit") int limit,
+//                                          @RequestParam("cursor") int cursor,
+//                                          @RequestParam("title") String title) {
+//        return shellService.search(limit, cursor, title);
+//    }
 
     private Long getId(Authentication authentication) {
         Long id;
@@ -98,7 +103,6 @@ public class ShellController {
             MemberContextInform memberInform = (MemberContextInform) authentication.getPrincipal();
             id = memberInform.getId();
         }
-        System.out.println("id = " + id);
         return id;
     }
 }
--- server/src/main/java/com/shellwe/server/domain/shell/entity/Shell.java
+++ server/src/main/java/com/shellwe/server/domain/shell/entity/Shell.java
@@ -2,21 +2,24 @@ package com.shellwe.server.domain.shell.entity;
 
 import com.shellwe.server.domain.category.entity.Category;
 import com.shellwe.server.domain.member.entity.Member;
+import com.shellwe.server.domain.picture.entity.Picture;
 import com.shellwe.server.domain.tag.entity.Tag;
 import com.shellwe.server.domain.types.ShellType;
 import com.shellwe.server.domain.types.Status;
 import com.shellwe.server.utils.TimeTracker;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 @Table(name = "SHELL")
 @Entity
 public class Shell extends TimeTracker {
@@ -38,22 +41,94 @@ public class Shell extends TimeTracker {
 
     private String body;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     @JoinColumn(name = "CATEGORY_ID")
     private Category category;
 
-    @OneToMany
+    @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "SHELL_ID")
     private List<Tag> tags = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "SHELL_ID")
+    private List<Picture> pictureUrls = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
+    public void statusActive() {
+        this.status = Status.ACTIVE;
+    }
+
+    public void statusInActive() {
+        this.status = Status.INACTIVE;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
     public void setMember(Member member) {
         this.member = member;
         if (!member.getShells().contains(this)) {
             member.getShells().add(this);
+        }
+    }
+
+    public void setPictureUrls(List<String> pictureUrls) {
+        List<Picture> pictures = IntStream.range(0, pictureUrls.size())
+                .mapToObj(order -> new Picture(order, pictureUrls.get(order)))
+                .collect(Collectors.toList());
+
+        this.pictureUrls = pictures;
+    }
+
+    public void addOnePictureUrl(String pictureUrl, int pictureOrder) {
+        boolean isOrderExist = this.pictureUrls.stream()
+                .anyMatch(picture -> picture.getOrder() == pictureOrder);
+        if (isOrderExist) {
+            throw new IllegalArgumentException("해당 순서의 사진이 이미 존재합니다.");
+        }
+        Picture picture = new Picture(pictureOrder, pictureUrl);
+        this.pictureUrls.add(picture);
+    }
+
+    public void updateShellPictureUrl(String pictureUrl, int pictureOrder) {
+        Picture picture = this.getPictureUrls().get(pictureOrder);
+        picture.updatePictureUrl(pictureOrder, pictureUrl);
+        this.getPictureUrls().set(pictureOrder, picture);
+    }
+
+    public void deleteOnePictureUrl(int pictureOrder) {
+        Picture deletePicture = this.pictureUrls.stream()
+                .filter(picture -> picture.getOrder() == pictureOrder)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("해당 순서의 사진이 존재하지 않습니다."));
+
+        this.pictureUrls.remove(deletePicture);
+        for (Picture remainingPicture : this.pictureUrls) {
+            if (remainingPicture.getOrder() > pictureOrder) {
+                remainingPicture.minusOrder();
+            }
+        }
+    }
+
+    public void updateShellInformExceptPictureUrl(Shell requestShell) {
+        if (requestShell.getTitle() != null) {
+            this.title = requestShell.getTitle();
+        }
+        if (requestShell.getBody() != null) {
+            this.body = requestShell.getBody();
+        }
+        if (requestShell.getShellType() != null) {
+            this.shellType = requestShell.getShellType();
+        }
+        if (requestShell.getCategory() != null) {
+            this.category = requestShell.getCategory();
+        }
+        if (requestShell.getTags() != null) {
+            this.tags = requestShell.getTags();
         }
     }
 }
