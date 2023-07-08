c06baca67794fadb4f93a2c4fc54c0ff7a4504cb
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-08 16:53:39 +0900

    [feat] EditProfile UI 완료 [FE] 마이 페이지 - 회원정보 수정 #27

---
+++ client/src/component/profile/EditProfile.styled.ts
@@ -0,0 +1,84 @@
+/* eslint-disable import/no-unresolved */
+import styled from 'styled-components';
+import { UserImg, Box } from './profile.styled.ts';
+import { SmallButton6 } from '../../common/button/Button.styled';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 50px;
+  width: 700px;
+  height: 700px;
+  background-color: #fff;
+  border-radius: 10px;
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+  padding-left: 50px;
+`;
+
+const ContentBox = styled(Box)``;
+
+const ProfileImage = styled(UserImg)`
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 150px;
+  height: 150px;
+`;
+
+const Title = styled.div`
+  font-size: 16px;
+`;
+
+const TextDiv = styled(Title)`
+  display: flex;
+  width: 100%;
+  height: 15%;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #023e8a;
+  color: #fff;
+  border-radius: 0px 0px 10px 10px;
+`;
+
+const NicknameInput = styled.input`
+  width: 320px;
+`;
+
+const IntroductionInput = styled(NicknameInput)`
+  height: 150px;
+  max-height: fit-content;
+`;
+
+const SaveButton = styled(SmallButton6)`
+  width: 125px;
+  height: 40px;
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+`;
+
+const CancelButton = styled(SaveButton)`
+  background-color: #fff;
+  color: #023e8a;
+`;
+
+const ButtonBox = styled(ContentBox)`
+  flex-direction: row;
+`;
+
+const Image = styled.img`
+  height: 85%; // img 밑에 텍스트인 "change picture" 요소의 높이를 15% 줬기 때문입니다.
+`;
+
+export {
+  Wrapper,
+  ProfileImage,
+  Title,
+  TextDiv,
+  ContentBox,
+  NicknameInput,
+  IntroductionInput,
+  SaveButton,
+  CancelButton,
+  ButtonBox,
+  Image,
+};
---
+++ client/src/component/profile/EditProfile.tsx
@@ -0,0 +1,43 @@
+import {
+  ProfileImage,
+  Wrapper,
+  Title,
+  TextDiv,
+  ContentBox,
+  NicknameInput,
+  IntroductionInput,
+  SaveButton,
+  CancelButton,
+  ButtonBox,
+  Image,
+} from './editProfile.styled';
+import example from '../../asset/example.png';
+
+const EditProfile = () => {
+  return (
+    <Wrapper>
+      <ContentBox>
+        <Title>profile image</Title>
+        <ProfileImage>
+          <Image src={example} alt="userimg" />
+          {/* 이미지는 js 작업 시, res 객체로 받은 데이터로 수정 */}
+          <TextDiv>change picture</TextDiv>
+        </ProfileImage>
+      </ContentBox>
+      <ContentBox>
+        <Title>Change nickname</Title>
+        <NicknameInput></NicknameInput>
+      </ContentBox>
+      <ContentBox>
+        <Title>Introduction</Title>
+        <IntroductionInput></IntroductionInput>
+      </ContentBox>
+      <ButtonBox>
+        <SaveButton>Save</SaveButton>
+        <CancelButton>Cancel</CancelButton>
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+export default EditProfile;
