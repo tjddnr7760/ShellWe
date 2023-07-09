2e7de6c418ba0942ea0cccaa45b08de9f7bb8d1e
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-09 10:38:52 +0900

    [refactor] MyPage, MyShellsPage 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import GlobalStyle from './style/GlobalStyle.ts';
-
-
 // import Header from './Components/Common/Header/Header';
 // import Sidebar from './Components/Common/Sidebar/Sidebar';
 // import Footer from './Components/Common/Footer/Footer';
@@ -12,13 +10,13 @@ import GlobalStyle from './style/GlobalStyle.ts';
 // import MyPage from './Pages/My/MyPage.jsx';
 // import SignupPage from './Pages/LoginSignup/SingupPage.jsx';
 // import AfterSignupPage from './Pages/LoginSignup/AfterSignupPage.jsx';
-
-
+// import MyPage from './page/mypage/MyPage.tsx';
+// import MyShellsPage from './page/myshells/MyShellsPage.tsx';
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
+      {/* <Header /> */}
       <GlobalStyle />
       <main>
         <Routes>
@@ -34,6 +32,8 @@ function App() {
           <Route path="/shellcreate" element={} /> // 제품 생성 페이지
           <Route path="/shelldetail/:id" element={} /> // 제품 상세 페이지
           <Route path="/shellupdate" element={} /> // 제품 수정 페이지
+          <Route path="/member/:id" element={<MyPage />} /> // 마이 페이지
+          <Route path="/myshells/:id" element={<MyShellsPage />} /> // 마이 쉘 페이지
           <Route path="/member/:id" element={} /> // 마이 페이지
           <Route path="/dm/:id" element={} /> // DM 페이지
           <Route path="/offer/:id" element={} /> // 요청함 페이지 */}
