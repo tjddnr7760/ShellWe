d95c2d933e78a17d00f9805604cb35e3244881c9
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-09 21:17:52 +0900

    [feat] ShellInfo UI 완료 #37

---
+++ client/src/component/offeredshells/ShellInfo.styled.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const ShellInfoContainer = styled.div`
+  display: flex;
+  width: 505px;
+  height: 135px;
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+  padding: 15px;
+  gap: 15px;
+`;
+
+const ImageBox = styled.div`
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  flex-grow: 1;
+  max-width: 105px;
+  overflow: hidden;
+`;
+
+const ShellImage = styled.img`
+  width: 100%;
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  align-items: center;
+  flex-grow: 1;
+  border: 0.5px solid rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
+  font-size: 20px;
+  padding-left: 10px;
+`;
+
+export { ShellInfoContainer, ImageBox, ShellImage, TextBox };
