6df2a52435cb2eb85be5e8bd1f90767ee756b93d
Mason3144 <lts890303@gmail.com>
2023-07-10 15:12:49 +0900

    [feat] 브랜치 연결 (#55)

--- websocket/src/main/java/com/shellwe/websocket/handler/CustomHandshakeHandler.java
+++ websocket/src/main/java/com/shellwe/websocket/handler/CustomHandshakeHandler.java
@@ -1,11 +1,8 @@
 package com.shellwe.websocket.handler;
 
 import org.springframework.http.server.ServerHttpRequest;
-import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.server.HandshakeFailureException;
-import org.springframework.web.socket.server.HandshakeHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import java.security.Principal;
