128198a52b690219b021605a2cdf912c6a1d1996
Mason3144 <lts890303@gmail.com>
2023-07-10 23:33:04 +0900

    [edit] Thread pool configuration (#55)

--- websocket/src/main/java/com/shellwe/websocket/service/Service.java
+++ websocket/src/main/java/com/shellwe/websocket/service/Service.java
@@ -2,10 +2,12 @@ package com.shellwe.websocket.service;
 
 
 import com.shellwe.websocket.auth.memberDetails.MemberContextInform;
+import com.shellwe.websocket.dto.ChatRoom;
 import com.shellwe.websocket.dto.MemberDto;
 import com.shellwe.websocket.dto.QueryDto;
 import com.shellwe.websocket.entity.Member;
 import com.shellwe.websocket.entity.MemberRoom;
+import com.shellwe.websocket.entity.Message;
 import com.shellwe.websocket.entity.Room;
 import com.shellwe.websocket.exception.businessLogicException.BusinessLogicException;
 import com.shellwe.websocket.exception.businessLogicException.ExceptionCode;
@@ -16,12 +18,15 @@ import com.shellwe.websocket.repository.MessageRepository;
 import com.shellwe.websocket.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import javax.transaction.Transactional;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
@@ -42,6 +47,7 @@ public abstract class Service {
         Optional<MemberRoom> optionalMemberRoom = memberRoomRepository.findByRoomAndMemberAndActiveTrue(new Room(roomId), new Member(memberId));
         return optionalMemberRoom.orElseThrow(()->new BusinessLogicException(ExceptionCode.MEMBER_ROOM_NOT_FOUND));
     }
+
     protected MemberDto.Response getMemberResponse(WebSocketSession session){
         Authentication authentication = (Authentication) session.getPrincipal();
         MemberContextInform member =  (MemberContextInform) authentication.getPrincipal();
