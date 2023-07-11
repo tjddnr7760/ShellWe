7268bd54ce434c903357bd071ca0e4646b836a58
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-11 17:40:11 +0900

    [refactor] mypage, myshells #25 #39

--- client/src/page/myshells/MyShellsPage.styled.ts
+++ client/src/page/myshells/MyShellsPage.styled.ts
@@ -2,19 +2,18 @@ import styled from 'styled-components';
 
 const MyShellsPageWrapper = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-start;
-  width: 700px;
-  padding: 35px;
+  flex-direction: row;
+  flex-grow: 1;
+  justify-content: center;
 `;
 
 const MyShellsPageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100%;
+  width: 735px;
   height: fit-content;
   gap: 10px;
+  padding: 35px;
 `;
 
 export { MyShellsPageWrapper, MyShellsPageContainer };
