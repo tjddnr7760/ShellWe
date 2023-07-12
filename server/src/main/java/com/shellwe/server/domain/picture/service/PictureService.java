a8335075425da297c12856623dd251a19360b205
cs2199 <tjddnr7760@naver.com>
2023-07-12 22:35:47 +0900

    [refactor] 파일 업로드 클래스 네이밍 수정

--- server/src/main/java/com/shellwe/server/domain/picture/service/PictureService.java
+++ server/src/main/java/com/shellwe/server/domain/picture/service/PictureService.java
@@ -1,18 +1,15 @@
 package com.shellwe.server.domain.picture.service;
 
-import com.shellwe.server.domain.member.entity.Member;
-import com.shellwe.server.domain.picture.entity.Picture;
-import com.shellwe.server.file.service.FileService;
+import com.shellwe.server.file.service.UploadPictureService;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class PictureService {
 
-    private final FileService fileService;
+    private final UploadPictureService uploadPictureService;
 
-    public PictureService(FileService fileService /*, add your Picture and Member repository here */) {
-        this.fileService = fileService;
+    public PictureService(UploadPictureService uploadPictureService /*, add your Picture and Member repository here */) {
+        this.uploadPictureService = uploadPictureService;
     }
 
 
