cdaf499ab3e3ad7dabf239d98ba39eeddb54ca88
cs2199 <tjddnr7760@naver.com>
2023-07-12 23:00:38 +0900

    [refactor] 머지 충돌 해결

--- server/src/main/java/com/shellwe/server/ServerApplication.java
+++ server/src/main/java/com/shellwe/server/ServerApplication.java
@@ -2,8 +2,6 @@ package com.shellwe.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
