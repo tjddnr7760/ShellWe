04f3d41adc55d54d6c5f5d5bb52e2a855eed38d6
cs2199 <tjddnr7760@naver.com>
2023-07-12 22:12:26 +0900

    [feat] 태그 도메인 구현

--- server/src/main/java/com/shellwe/server/domain/tag/entity/Tag.java
+++ server/src/main/java/com/shellwe/server/domain/tag/entity/Tag.java
@@ -3,11 +3,9 @@ package com.shellwe.server.domain.tag.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import javax.persistence.*;
 
-@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
