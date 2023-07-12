040e73c71def06d8ab69b8e7526c5c4b7065507a
SEB_FE_44_이호섭 <34961388+lhs9602@users.noreply.github.com>
2023-07-12 13:45:45 +0900

    [feat]api 요청 객체화랑 설명서 추가 [FE] 비동기 작업 #34

--- client/src/hooks/useMutionRequest.ts
+++ client/src/hooks/useMutionRequest.ts
@@ -1,35 +1,18 @@
 import { useMutation } from 'react-query';
 import { sendAxiosRequest } from '../utill/sendAxiosRequest.ts';
-const DOMAIN = `${import.meta.env.VITE_KEY}`;
+import { RequestConfigArgs } from '../utill/requestConfig.ts';
 
 export const useMutionRequest = (
-  apiUrl: string,
-  methodType: string,
-  requestData: null | object | FormData = null,
-  isHeader: boolean,
-  onSuccess?: (responseData: any) => void // onSuccess 매개변수 추가
+  requestConfig: RequestConfigArgs,
+  onSuccess?: (responseData: object) => void // onSuccess 매개변수 추가
 ) => {
-  const requestHeaders = isHeader
-    ? { Authorization: 'Bearer your_access_token' }
-    : null;
-
-  const requestMutionConfig = {
-    url: `${DOMAIN}${apiUrl}`,
-    method: methodType,
-    data: requestData,
-    headers: requestHeaders,
-  };
-
-  const { data, mutate } = useMutation(
-    () => sendAxiosRequest(requestMutionConfig),
-    {
-      onSuccess: (responseData) => {
-        if (onSuccess) {
-          onSuccess(responseData); // 전달된 onSuccess 콜백 실행
-        }
-      },
-    }
-  );
+  const { data, mutate } = useMutation(() => sendAxiosRequest(requestConfig), {
+    onSuccess: (responseData) => {
+      if (onSuccess) {
+        onSuccess(responseData); // 전달된 onSuccess 콜백 실행
+      }
+    },
+  });
 
   return { data, mutate };
 };
