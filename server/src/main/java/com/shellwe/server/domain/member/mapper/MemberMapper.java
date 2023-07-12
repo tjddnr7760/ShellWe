608a687210a8e93dbe2819662e42c9504857c8e5
cs2199 <tjddnr7760@naver.com>
2023-07-12 22:11:49 +0900

    [feat] 멤버 엔티티, 메퍼, 서비스 레이어 구현

--- server/src/main/java/com/shellwe/server/domain/member/mapper/MemberMapper.java
+++ server/src/main/java/com/shellwe/server/domain/member/mapper/MemberMapper.java
@@ -4,7 +4,16 @@ import com.shellwe.server.domain.member.dto.request.SignUpRequestDto;
 import com.shellwe.server.domain.member.dto.request.UpdateRequestDto;
 import com.shellwe.server.domain.member.dto.response.FindResponseDto;
 import com.shellwe.server.domain.member.entity.Member;
+import com.shellwe.server.domain.picture.dto.PictureResponseDto;
+import com.shellwe.server.domain.picture.entity.Picture;
+import com.shellwe.server.domain.shell.dto.response.ShellResponseDto;
+import com.shellwe.server.domain.shell.entity.Shell;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface MemberMapper {
@@ -14,4 +23,18 @@ public interface MemberMapper {
     Member updateRequestDtoToMember(UpdateRequestDto updateRequestDto);
 
     FindResponseDto memberToFindResponseDto(Member member);
+
+    List<ShellResponseDto> shellListToGetMyShellListDto(List<Shell> activeShellList);
+
+    @Mapping(target="picture", expression="java(mapFirstPictureToDto(shell.getPictureUrls()))")
+    @Mapping(target="type", source="shell.shellType")
+    @Mapping(target="category", source="shell.category.shellCategory")
+    ShellResponseDto shellToShellResponseDto(Shell shell);
+
+    default String mapFirstPictureToDto(List<Picture> pictures) {
+        if (pictures != null && !pictures.isEmpty()) {
+            return pictures.get(0).getUrl();
+        }
+        return null;
+    }
 }
