7b15fa7d460bcb0b3f18b7134cc86b23955a577d
cs2199 <tjddnr7760@naver.com>
2023-07-13 17:50:35 +0900

    [feat] 런타임 에러, 컨트롤러 에러 추가

--- server/src/main/java/com/shellwe/server/exception/handler/ControllerExceptionHandler.java
+++ server/src/main/java/com/shellwe/server/exception/handler/ControllerExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.shellwe.server.exception.handler;
 
-import com.nimbusds.oauth2.sdk.ErrorResponse;
 import com.shellwe.server.exception.response.ControllerErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,15 +8,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ControllerErrorResponse handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException e) {
-        log.error("예외 발생 : ", e);
-        return ControllerErrorResponse.of(e.getBindingResult());
+    public ControllerErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException) {
+        log.error("controller valid error", methodArgumentNotValidException);
+        return ControllerErrorResponse.of(methodArgumentNotValidException.getBindingResult());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ControllerErrorResponse handleConstraintViolationException(ConstraintViolationException constraintViolationException) {
+        log.error("controller valid error", constraintViolationException);
+        return ControllerErrorResponse.of(constraintViolationException.getConstraintViolations());
     }
 }
--- server/src/main/java/com/shellwe/server/exception/handler/RuntimeExceptionHandler.java
+++ server/src/main/java/com/shellwe/server/exception/handler/RuntimeExceptionHandler.java
@@ -3,7 +3,6 @@ package com.shellwe.server.exception.handler;
 import com.shellwe.server.exception.response.RuntimeErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,11 +12,7 @@ public class RuntimeExceptionHandler {
 
     @ExceptionHandler
     public RuntimeErrorResponse handleRuntimeException(RuntimeException e) {
-        log.error("예외 발생 : ", e);
-
-        RuntimeErrorResponse runtimeErrorResponse = new RuntimeErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
-
-        return runtimeErrorResponse;
+        log.error("runtime exception", e);
+        return new RuntimeErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage());
     }
-
 }
