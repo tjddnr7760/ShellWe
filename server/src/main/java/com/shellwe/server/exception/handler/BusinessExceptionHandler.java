5c0bb6a1d096243438ec308250d4f8bd6047c08c
cs2199 <tjddnr7760@naver.com>
2023-07-13 10:57:47 +0900

    [feat] 예외 처리 클래스 구조화

---
+++ server/src/main/java/com/shellwe/server/exception/handler/BusinessExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.shellwe.server.exception.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class BusinessExceptionHandler {
+
+}
