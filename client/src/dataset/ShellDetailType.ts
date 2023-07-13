dac0ddccaadb2b169b6a1c8fd7366d4f9bafeed8
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-13 21:10:14 +0900

    [feat] 타입 정리 [FE] 제품 상세 페이지 - 조회 #14

---
+++ client/src/dataset/ShellDetailType.ts
@@ -0,0 +1,60 @@
+// MyShellList, MyShell of OfferModal
+export interface MyShellsDataProps {
+  myShellsData: MyShells[];
+}
+
+export interface MyShells {
+  id: number;
+  type: string;
+  status: string;
+  title: string;
+  createdAt: string;
+  category: string;
+  picture: string;
+  member: {
+    id: number;
+    displayName: string;
+    profileUrl: string;
+  };
+}
+
+// ShellDetail
+export interface ShellDetailProps {
+  handlePoke: () => void; // Define the handlePoke prop
+  handleOpenSidebar: () => void;
+  shellDetailData: ShellDetailDataProps;
+}
+
+export interface ShellDetailDataProps {
+  id: number;
+  type: string;
+  title: string;
+  pictures: Picture[];
+  body: string;
+  category: string;
+  createdAt: string;
+  member: Member;
+  modifiedAt: string;
+  status: string;
+  tags: Tag[];
+}
+
+export interface Picture {
+  order: number;
+  url: string;
+}
+
+export interface Tag {
+  tagName: string;
+}
+
+export interface Member {
+  id: number;
+  displayName: string;
+  profileUrl: string;
+}
+
+export interface BodyProps {
+  category: string;
+  body: string;
+}
