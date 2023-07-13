6c268072590acb940ac280b9dc8b23470b7335ea
Cishcash8725 <cishcash8725@gmail.com>
2023-07-13 16:23:51 +0900

    RuntimeException 및 유효성Exception Handle

--- server/src/main/java/com/shellwe/server/exception/handler/RuntimeExceptionHandler.java
+++ server/src/main/java/com/shellwe/server/exception/handler/RuntimeExceptionHandler.java
@@ -1,10 +1,23 @@
 package com.shellwe.server.exception.handler;
 
+import com.shellwe.server.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class RuntimeExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<ErrorResponse> handleRuntimeException(RuntimeException e) {
+        log.error("예외 발생 : ", e);
+
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }
