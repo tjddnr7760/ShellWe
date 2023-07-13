4b74c92b528afb757ff1429069872bda235df5ff
cs2199 <tjddnr7760@naver.com>
2023-07-13 16:35:43 +0900

    [refactor] 머지전 수정

--- server/src/main/java/com/shellwe/server/exception/handler/ControllerExceptionHandler.java
+++ server/src/main/java/com/shellwe/server/exception/handler/ControllerExceptionHandler.java
@@ -2,18 +2,15 @@ package com.shellwe.server.exception.handler;
 
 import com.shellwe.server.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @RestControllerAdvice
-public class ValidationExceptionHandler {
+public class ControllerExceptionHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
