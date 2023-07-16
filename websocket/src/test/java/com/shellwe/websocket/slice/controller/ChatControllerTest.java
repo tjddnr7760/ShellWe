3d716114158b644df5982a5b76e7f0021c14dc15
Mason3144 <lts890303@gmail.com>
2023-07-16 10:14:53 +0900

    [test] API documented with RestDocs for ChatController(Http Controller) (#91)

--- websocket/src/test/java/com/shellwe/websocket/slice/controller/ChatControllerTest.java
+++ websocket/src/test/java/com/shellwe/websocket/slice/controller/ChatControllerTest.java
@@ -3,9 +3,11 @@ package com.shellwe.websocket.slice.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shellwe.websocket.controller.ChatController;
 import com.shellwe.websocket.customAnnotation.WithMockCustomUser;
+import com.shellwe.websocket.dto.MemberDto;
 import com.shellwe.websocket.dto.ResponseDto;
 import com.shellwe.websocket.dto.RoomDto;
 import com.shellwe.websocket.service.HttpService;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +22,30 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import org.springframework.restdocs.payload.JsonFieldType;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.mockito.BDDMockito.given;
 
@@ -44,16 +60,58 @@ public class ChatControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    private static Map<String,String> headers = new HashMap<>();
+
+    @BeforeAll
+    static void init() {
+        headers.put("Authorization","Bearer qwmdkqlmdkaend.qmnedkmqwkdmqkwmdk...");
+        headers.put("Refresh","qdwnjqnwdjc.ewhsndjnq...");
+    }
+
     @Test
     @WithMockCustomUser
     void getRoomTest() throws Exception {
-        given(httpService.findAllRoom()).willReturn(List.of(RoomDto.Response.builder().build()));
+        MemberDto.Response memberResponse =  MemberDto.Response.builder().id(1L).profileUrl("empty").displayName("홍길동").build();
+
+        RoomDto.Response roomResponse = RoomDto.Response.builder()
+                .id(1L)
+                .unread(1L)
+                .lastMessage("마지막 메세지")
+                .member(memberResponse)
+                .build();
+
+        given(httpService.findAllRoom()).willReturn(List.of(roomResponse));
 
         ResultActions actions = mockMvc.perform(
                 get("/chat")
+                        .header("Authorization", headers.get("Authorization"))
+                        .header("Refresh", headers.get("Refresh"))
         );
 
-        actions.andExpect(status().isOk());
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.[0].id").value(roomResponse.getId()))
+                .andExpect(jsonPath("$.[0].unread").value(roomResponse.getUnread()))
+                .andExpect(jsonPath("$.[0].lastMessage").value(roomResponse.getLastMessage()))
+                .andExpect(jsonPath("$.[0].member.id").value(roomResponse.getMember().getId()))
+                .andExpect(jsonPath("$.[0].member.profileUrl").value(roomResponse.getMember().getProfileUrl()))
+                .andExpect(jsonPath("$.[0].member.displayName").value(roomResponse.getMember().getDisplayName()))
+
+                .andDo(document("get-rooms",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access token").attributes(key("constraints").value("Start with 'Bearer'")),
+                                headerWithName("Refresh").description("Refresh token")
+                        ),
+                        responseFields(
+                                fieldWithPath("[].id").type(JsonFieldType.NUMBER).description("룸 식별자"),
+                                fieldWithPath("[].unread").type(JsonFieldType.NUMBER).description("읽지 않은 메세지 수"),
+                                fieldWithPath("[].lastMessage").type(JsonFieldType.STRING).description("마지막 메세지"),
+                                fieldWithPath("[].member.id").type(JsonFieldType.NUMBER).description("채팅 상대 멤버 식별자"),
+                                fieldWithPath("[].member.profileUrl").type(JsonFieldType.STRING).description("채팅 상대 멤버 사진 URL"),
+                                fieldWithPath("[].member.displayName").type(JsonFieldType.STRING).description("채팅 상대 멤버 이름")
+                        )
+                ));
     }
 
     @Test
@@ -62,17 +120,40 @@ public class ChatControllerTest {
         ResultActions actions = mockMvc.perform(
                 delete("/chat/{roomId}",1L)
                         .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .header("Authorization", headers.get("Authorization"))
+                        .header("Refresh", headers.get("Refresh"))
         );
-
-        actions.andExpect(status().isNoContent());
+        actions.andExpect(status().isNoContent())
+                .andDo(document("delete-room",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access token").attributes(key("constraints").value("Start with 'Bearer'")),
+                                headerWithName("Refresh").description("Refresh token")
+                        ),
+                        pathParameters(
+                                parameterWithName("roomId").description("댓글 식별자")
+                        )
+                ));
     }
 
     @Test
     @WithMockCustomUser
     void createRoomTest() throws Exception {
-        String content = objectMapper.writeValueAsString(RoomDto.Post.builder().build());
+        RoomDto.Post roomDto = RoomDto.Post.builder()
+                .myShellId(1L)
+                .sellerShellId(2L)
+                .sellerMemberId(2L)
+                .build();
 
-        given(httpService.createRoom(Mockito.any())).willReturn(ResponseDto.builder().build());
+        String content = objectMapper.writeValueAsString(roomDto);
+
+        ResponseDto response = ResponseDto.builder()
+                .roomsUrl("http://localhost:8080/chat")
+                .roomUrl("ws://localhost:8080/chat?roomId=1")
+                .build();
+
+        given(httpService.createRoom(Mockito.any())).willReturn(response);
 
         ResultActions actions = mockMvc.perform(
                 post("/chat")
@@ -80,9 +161,30 @@ public class ChatControllerTest {
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(content)
+                        .header("Authorization", headers.get("Authorization"))
+                        .header("Refresh", headers.get("Refresh"))
         );
 
-        actions.andExpect(status().isCreated());
-    }
+        actions.andExpect(status().isCreated())
+                .andExpect(jsonPath("$.roomsUrl").value(response.getRoomsUrl()))
+                .andExpect(jsonPath("$.roomUrl").value(response.getRoomUrl()))
 
+                .andDo(document("post-room",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Authorization").description("Access token").attributes(key("constraints").value("Start with 'Bearer'")),
+                                headerWithName("Refresh").description("Refresh token")
+                        ),
+                        requestFields(
+                                fieldWithPath("myShellId").type(JsonFieldType.NUMBER).description("거래할 나의 쉘 식별자"),
+                                fieldWithPath("sellerShellId").type(JsonFieldType.NUMBER).description("거래할 상대방의 쉘 식별자"),
+                                fieldWithPath("sellerMemberId").type(JsonFieldType.NUMBER).description("거래할 대상의 멤버 식별자")
+                        ),
+                        responseFields(
+                                fieldWithPath("roomsUrl").type(JsonFieldType.STRING).description("현재 멤버의 활성화된 모든 채팅방 조회 API URL"),
+                                fieldWithPath("roomUrl").type(JsonFieldType.STRING).description("생성된 채팅방 연결 웹소켓 URL")
+                        )
+                ));
+    }
 }
