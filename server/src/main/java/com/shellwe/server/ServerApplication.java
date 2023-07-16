50b30c7f832aee0b38a9a463d26fadc19e999b47
Mason3144 <lts890303@gmail.com>
2023-07-16 11:10:51 +0900

    [test] origin BE와 rebase 후 merge 작업

--- server/src/main/java/com/shellwe/server/ServerApplication.java
+++ server/src/main/java/com/shellwe/server/ServerApplication.java
@@ -11,7 +11,6 @@ public class ServerApplication {
 	static {
 		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
 	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
