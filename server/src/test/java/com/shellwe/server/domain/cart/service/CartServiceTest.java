c1ab37e432a4223c570e0a0876b1f8be8e8aaeec
cs2199 <tjddnr7760@naver.com>
2023-07-17 11:30:13 +0900

    [test] 테스트 코드 작성중

--- server/src/test/java/com/shellwe/server/domain/cart/service/CartServiceTest.java
+++ server/src/test/java/com/shellwe/server/domain/cart/service/CartServiceTest.java
@@ -1,4 +1,28 @@
+package com.shellwe.server.domain.cart.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CartServiceTest {
-  
+
+    @Test
+    void saveShellInMyCart() {
+    }
+
+    @Test
+    void deleteShellInMyCart() {
+    }
+
+    @Test
+    void getMyCartShells() {
+    }
+
+    @Test
+    void handleShellRemoveEvent() {
+    }
+
+    @Test
+    void handleMemberRemoveEvent() {
+    }
 }
