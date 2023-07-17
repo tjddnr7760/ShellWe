0bb57c0eee61f23b75096bf24c04a3db4be92d18
cs2199 <tjddnr7760@naver.com>
2023-07-17 11:29:32 +0900

    [feat] 예외처리 추가

--- server/src/main/java/com/shellwe/server/exception/customexception/AccessTokenException.java
+++ server/src/main/java/com/shellwe/server/exception/customexception/AccessTokenException.java
@@ -1,2 +1,15 @@
-package com.shellwe.server.exception.customexception;public class AccessTokenException {
+package com.shellwe.server.exception.customexception;
+
+import com.shellwe.server.exception.exceptioncode.AccessTokenExceptionCode;
+import lombok.Getter;
+
+@Getter
+public class AccessTokenException extends RuntimeException {
+
+    private AccessTokenExceptionCode accessTokenExceptionCode;
+
+    public AccessTokenException(AccessTokenExceptionCode accessTokenExceptionCode) {
+        super(accessTokenExceptionCode.getMessage());
+        this.accessTokenExceptionCode = accessTokenExceptionCode;
+    }
 }
