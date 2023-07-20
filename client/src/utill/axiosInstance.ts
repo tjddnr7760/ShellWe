50f16182866e72b926d0c2984e97593f558d1f46
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-20 09:20:27 +0900

    [fix] env, axios

--- client/src/utill/axiosInstance.ts
+++ client/src/utill/axiosInstance.ts
@@ -4,37 +4,19 @@ import { v4 as uuidv4 } from 'uuid';
 export const getHeader = () => {
   return {
     'ngrok-skip-browser-warning': '69420',
-    Authorization: 'Bearer eyJhbGciOiJIUzM4NCJ9.eyJkaXNwbGF5TmFtZSI6InRqZGRuciIsImlkIjoxLCJlbWFpbFZlcmlmaWNhdGlvblN0YXR1cyI6dHJ1ZSwic3ViIjoidGpkZG5yNzc2MEBuYXZlci5jb20iLCJpYXQiOjE2ODk2Njk5OTMsImV4cCI6MTY5MDI2OTk5M30.quApNU49HAmBHpm4jr2FC-BKHfpNY0m5TjcQzJ8EYxayGuByX5lLONRvASbQxujW',
+    Authorization:
+      'Bearer eyJhbGciOiJIUzM4NCJ9.eyJkaXNwbGF5TmFtZSI6InRqZGRuciIsImlkIjoxLCJlbWFpbFZlcmlmaWNhdGlvblN0YXR1cyI6dHJ1ZSwic3ViIjoidGpkZG5yNzc2MEBuYXZlci5jb20iLCJpYXQiOjE2ODk2Njk5OTMsImV4cCI6MTY5MDI2OTk5M30.quApNU49HAmBHpm4jr2FC-BKHfpNY0m5TjcQzJ8EYxayGuByX5lLONRvASbQxujW',
   };
 };
 
-export const getPostHeader = () => {
-  return {
-    'ngrok-skip-browser-warning': '69420',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzM4NCJ9.eyJkaXNwbGF5TmFtZSI6InRqZGRuciIsImlkIjoxLCJlbWFpbFZlcmlmaWNhdGlvblN0YXR1cyI6dHJ1ZSwic3ViIjoidGpkZG5yNzc2MEBuYXZlci5jb20iLCJpYXQiOjE2ODk2Njk5OTMsImV4cCI6MTY5MDI2OTk5M30.quApNU49HAmBHpm4jr2FC-BKHfpNY0m5TjcQzJ8EYxayGuByX5lLONRvASbQxujW',
-    'Content-Type': `multipart/form-data; boundary=<${uuidv4}>`,
-  };
-};
 export const getPostHeader = () => {
   return {
     'ngrok-skip-browser-warning': '69420',
     Authorization:
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJkaXNwbGF5TmFtZSI6InRqZGRuciIsImlkIjoxLCJlbWFpbFZlcmlmaWNhdGlvblN0YXR1cyI6dHJ1ZSwic3ViIjoidGpkZG5yNzc2MEBuYXZlci5jb20iLCJpYXQiOjE2ODkzNDgxMzEsImV4cCI6MTY4OTc4MDEzMX0.0XS7OQKM2uSRp6jfeoQKRnptdhxkXlOG7hcq0IVXoUE',
     'Content-Type': `multipart/form-data; boundary=<${uuidv4}>`,
-
   };
 };
 const config: AxiosRequestConfig = { baseURL: `${import.meta.env.VITE_KEY}` };
 
 export const axiosInstance = axios.create(config);
-
-/*
-사용법    
- await axiosInstance({
-    url: urlEndpoint,
-    method: 'POST',
-    data: { email, password },
-    headers: getHeader,
-});
-*/
