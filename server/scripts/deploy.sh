cbf4412c0a0d8c92bce3238de46245e48b840f42
cs2199 <tjddnr7760@naver.com>
2023-07-20 14:48:04 +0900

    [feat] 배포 로직 수정

--- server/scripts/deploy.sh
+++ server/scripts/deploy.sh
@@ -25,5 +25,5 @@ fi
 
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/action/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/server/deploy.log
+sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/server/deploy.log 2>/home/ubuntu/server/deploy_err.log &
