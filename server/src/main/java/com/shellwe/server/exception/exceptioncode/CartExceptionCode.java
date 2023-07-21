4b74a3fbc9d6204e4e83072d898f5c7d2e2c0e8e
cs2199 <tjddnr7760@naver.com>
2023-07-21 13:58:23 +0900

    [refactor] 코드 정리

--- server/src/main/java/com/shellwe/server/exception/exceptioncode/CartExceptionCode.java
+++ server/src/main/java/com/shellwe/server/exception/exceptioncode/CartExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum CartExceptionCode {
 
-    CART_ALREADY_EXISTS(400, "Cart Duplicated");
+    CART_ALREADY_EXISTS(400, "Cart Duplicated"),
+    CART_DELETE_FAILED(400, "Cart Delete Failed");
 
     private int status;
 
