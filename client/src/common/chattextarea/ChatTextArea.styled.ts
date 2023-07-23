25944394039ec8ee0b0f01d79d5ab5e946de6087
Dongwoo Joo <wnehddn1111@gmail.com>
2023-07-24 01:23:31 +0900

    [fix] DM 페이지 수정

--- client/src/common/chattextarea/ChatTextArea.styled.ts
+++ client/src/common/chattextarea/ChatTextArea.styled.ts
@@ -57,8 +57,9 @@ export const SendButton = styled.button`
   }
 
   @media (max-width: 768px) {
-    width: 30px;
-    height: 30px;
+    width: 40px;
+    height: 40px;
     margin-right: 3px;
+    margin: 3px;
   }
 `;
